2f05e1fb26ac00bf42280360656f3b9042b0831b
Evgeniy Gul <gulevgeniy@gmail.com>
2019-07-18 13:53:28 +0300

    --- Big refactoring --- All classes rewritten  - New IntentService RepeatIntentService      instead old RepeatService

--- app/src/main/java/home/blackharold/simple_tracker/MainActivity.java
+++ app/src/main/java/home/blackharold/simple_tracker/MainActivity.java
@@ -1,10 +1,14 @@
 package home.blackharold.simple_tracker;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,6 +16,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import home.blackharold.simple_tracker.receivers.AlarmTrack;
+import home.blackharold.simple_tracker.receivers.BootReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +27,17 @@ public class MainActivity extends AppCompatActivity {
     EditText trackNameText;
     EditText timeoutText;
 
-    static String trackerName;
-    static int timeout = 30;
+    public static String trackerName;
+    public static int timeout = 30;
 
     //    Location
     SharedPreferences preferences;
+    AlarmManager alarmManager, bootManager;
+    Intent alarmIntent, bootIntent;
+    PendingIntent pendingAlarmIntent, pendingBootIntent;
+
+    //    permissions
+
 
     @TargetApi(Build.VERSION_CODES.M)
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -36,18 +50,51 @@ public class MainActivity extends AppCompatActivity {
         trackNameText = findViewById(R.id.trackNameText);
         timeoutText = findViewById(R.id.timeoutText);
 
+        final String[] INITIAL_PERMS = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        final int INITIAL_REQUEST = 4;
+//        AlarmManager for all
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+//        Intents for AlarmTrack
+        alarmIntent = new Intent(this, AlarmTrack.class);
+        pendingAlarmIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+//        Intents for BootReceiver
+//        Sent alarmIntent
+        bootIntent = new Intent(this, BootReceiver.class);
+        pendingBootIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
         start.setOnClickListener((view) -> {
             trackerName = trackNameText.getText().toString();
             timeout = Integer.parseInt(timeoutText.getText().toString());
-
-            preferences = getSharedPreferences("track_prefs", Context.MODE_MULTI_PROCESS);
+//            TODO change mode
+            preferences = getSharedPreferences("track_prefs", Activity.MODE_MULTI_PROCESS);
             SharedPreferences.Editor editor = preferences.edit();
             editor.putString("name", trackerName);
             editor.putInt("timeout", timeout);
             editor.apply();
-//            editor.commit();
+//            Alarms set:
+//            for AlarmTrack
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), timeout * 1000, pendingAlarmIntent);
+//            for BootReceiver
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), timeout * 1000, pendingBootIntent);
 
-            startService(new Intent(this, RepeatService.class));
+//            startService(new Intent(this, RepeatService.class));
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, INITIAL_REQUEST);
+            }
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, INITIAL_REQUEST);
+            }
+
             Toast.makeText(this, "Task starting...", Toast.LENGTH_LONG).show();
             finish();
         });
--- app/src/main/java/home/blackharold/simple_tracker/utils/QueryBuilder.java
+++ app/src/main/java/home/blackharold/simple_tracker/utils/QueryBuilder.java
@@ -1,23 +1,27 @@
-package home.blackharold.simple_tracker;
+package home.blackharold.simple_tracker.utils;
 
 import android.location.Location;
 import android.util.Log;
 
-import static home.blackharold.simple_tracker.MainActivity.trackerName;
-import static home.blackharold.simple_tracker.RepeatService.TAG;
+import static home.blackharold.simple_tracker.services.RepeatIntentService.TAG;
 
 public class QueryBuilder {
     Location location;
+    String trackerName;
+    float latitude = 0, longitude = 0;
 
     public QueryBuilder() {
         this.location = location;
     }
 
-    public String buildUrl(Location location) {
+    public String buildUrl(Location location, String trackerName) {
         String address, lat, lot, gmapUrl, result;
         StringBuilder builder = new StringBuilder();
-        float latitude = (float) location.getLatitude();
-        float longitude = (float) location.getLongitude();
+        if (location != null) {
+            latitude = (float) location.getLatitude();
+            longitude = (float) location.getLongitude();
+        }
+        this.trackerName = trackerName;
 
         address = "https://dweet.io/dweet/for/" + trackerName + "?";
         lat = "latitude=" + latitude;
